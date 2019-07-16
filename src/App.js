32a2eadb4c8c720cec931337a30a339e73e7b7e9
viostanj <viostanojguerrier@gmail.com>
2019-07-15 23:34:53 -0400

    get and post data OK : -> prepare to edit

--- src/App.js
+++ src/App.js
@@ -2,32 +2,59 @@ import React,{Component} from 'react';
 import {Table,Button, ModalHeader, ModalBody, ModalFooter, FormGroup, Label} from 'reactstrap';
 import axios from 'axios';
 import Modal from 'reactstrap/lib/Modal';
-import Input from 'reactstrap/lib/Input';
+import MyInput from './Input/Input.js';
 
 class App extends Component {
   state = {
     contact : {
-      nom : '',
-      prenom : '',
-      datenais : '',
-      phone : '',
-      email : '',]
-      adress : ''
+      nom:'',
+      prenom:'',
+      phone:'',
+      mail:'',
+      adress:'',
+      date:''
     },
     contacts : [],
     newContactModal : false
   }
 
-  //WARNING! To be deprecated in React v17. Use componentDidMount instead.
-  componentWillMount() {
+  componentDidMount() {
     axios.get('http://localhost/www/CloudContact/api/v1/contacts').then((response) =>{
-      console.log(response.data);
       this.setState({contacts : response.data});
     });
   }
 
   openContactModal = () =>{
     this.setState({newContactModal:!this.state.newContactModal});
+  }
+
+  editContact(contact){
+    console.log(contact);
+    
+  }
+
+  addContact() {
+      axios.post('http://localhost/www/CloudContact/api/v1/contacts/add',this.state.contact).then(response => {
+        console.log(response);
+        this.setState({
+          contact : {
+            nom:'',
+            prenom:'',
+            phone:'',
+            mail:'',
+            adress:'',
+            date:''
+          },
+          contacts : response.data,
+          newContactModal : false
+        });
+      });
+  }
+
+  handleValue(event,id){
+    let newState = {...this.state.contact};
+    newState[id] = event.target.value;
+    this.setState({contact : newState});
   }
 
   render() {
@@ -37,12 +64,12 @@ class App extends Component {
           <td>{contact.id}</td>
           <td>{contact.nom}</td>
           <td>{contact.prenom}</td>
-          <td>{contact.datenais}</td>
           <td>{contact.phone}</td>
           <td>{contact.email}</td>
           <td>{contact.adress}</td>
+          <td>{contact.datenais}</td>
           <td>
-            <Button color="success" size="sm" className="mr-2">Edit</Button>
+            <Button color="success" size="sm" className="mr-2" onClick={this.editContact.bind(this,contact)}>Edit</Button>
             <Button color="danger" size="sm">Delete</Button>
           </td>
         </tr>
@@ -58,33 +85,33 @@ class App extends Component {
 
     return (
         <div className="App container">
+          <h1 className="text-center">Contact React</h1>
           <Button color="primary" className="mr-4" onClick={this.openContactModal.bind(this)} style={btnStyle}>Add Contact</Button>
           <Modal isOpen={this.state.newContactModal}>
               <ModalHeader>Create Contact</ModalHeader>
               <ModalBody>
-                <FormGroup>
-                  <Input id="nom" placeholder="Nom"/>
-                </FormGroup>
-                <FormGroup>
-                  <Input id="prenom" placeholder="Prenom"/>
-                </FormGroup>
                 
-                <FormGroup>
-                  <Input id="phone" placeholder="Phone"/>
-                </FormGroup>
-                <FormGroup>
-                  <Input id="email" placeholder="E-mail" type="email"/>
-                </FormGroup>
-                <FormGroup>
-                  <Label  for="datenais">Date de Naissance</Label>
-                  <Input id="datenais" type="date"/>
-                </FormGroup>
-                <FormGroup>
-                  <Input id="adress" placeholder="Adresse"/>
-                </FormGroup>
+                <MyInput typ="text" hint="Nom" id="nom" val={this.state.contact.nom} 
+                  change={(event) => this.handleValue(event,'nom')}/>
+
+                <MyInput typ="text" hint="Prenom" id="prenom" val={this.state.contact.prenom} 
+                  change={(event) => this.handleValue(event,'prenom')}/>
+                  
+                <MyInput typ="number" hint="Phone" id="phone" val={this.state.contact.phone}
+                  change={(event) => this.handleValue(event,'phone')}/>
+
+                <MyInput typ="email" hint="E-mail" id="email" val={this.state.contact.mail}
+                  change={(event) => this.handleValue(event,'mail')}/>
+
+                <MyInput typ="text" hint="Adress" id="adress" val={this.state.contact.adress}
+                  change={(event) => this.handleValue(event,'adress')}/>
+
+                <MyInput typ="date" id="datenais" val={this.state.contact.date}
+                  change={(event) => this.handleValue(event,'date')}/>
+
               </ModalBody>
               <ModalFooter>
-                <Button color="success" onClick={this.openContactModal.bind(this)}>Save</Button>
+                <Button color="success" onClick={this.addContact.bind(this)}>Save</Button>
                 <Button color="primary" onClick={this.openContactModal.bind(this)}>Cancel</Button>
               </ModalFooter>
           </Modal>
@@ -94,10 +121,10 @@ class App extends Component {
                 <th>#</th>
                 <th>Nom</th>
                 <th>Prenom</th>
-                <th>Date Naissance</th>
                 <th>Phone</th>
                 <th>Enail</th>
                 <th>Adress</th>
+                <th>Date Naissance</th>
               </tr>
             </thead>
             <tbody>
---
+++ src/Input/Input.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {FormGroup,Label} from 'reactstrap';
+import InputB from 'reactstrap/lib/Input';
+
+class Input extends React.Component{
+
+    render() {
+        return (
+        <FormGroup>
+            <InputB id={this.props.id} placeholder={this.props.hint} type={this.props.typ} 
+                value={this.props.val} onChange={this.props.change}/>
+        </FormGroup>
+        );
+    }
+}
+
+export default Input;
